0c03c1f1996a9a1cd9aa25c9ac8e06149965d4fe
lucas machado <lucasmachadocbez@gmail.com>
2025-06-02 22:46:55 -0300

    Codigo com os 7 servos funcionando

--- bracoRoboticoCodigo_1/bracoRoboticoCodigo_1.ino
+++ bracoRoboticoCodigo_1/bracoRoboticoCodigo_1.ino
@@ -1,7 +1,7 @@
 #include <ESP32Servo.h>
 
-const int NUM_SERVOS = 6;
-const int pinos[NUM_SERVOS] = {18, 19, 22, 5, 25,26}; // GPIOs conectados aos servos
+const int NUM_SERVOS = 7;
+const int pinos[NUM_SERVOS] = {18, 19, 22, 23, 5, 25,26}; // GPIOs conectados aos servos
 Servo servos[NUM_SERVOS];
 
 void setup() {
@@ -14,7 +14,7 @@ void setup() {
 
 void loop() {
 
-  for (int angulo = 0; angulo <= 90; angulo++) {
+  for (int angulo = 0; angulo <= 60; angulo++) {
     for (int i = 0; i < NUM_SERVOS; i++) {
       servos[i].write(angulo);
     }
@@ -24,11 +24,11 @@ void loop() {
   delay(1000); // Pequena pausa
 
   
-  for (int angulo = 90; angulo >= 0; angulo--) {
+  for (int angulo = 60; angulo >= 0; angulo--) {
     for (int i = 0; i < NUM_SERVOS; i++) {
       servos[i].write(angulo);
     }
-    delay(50);
+    delay(100);
   }
 
   delay(1000); // Outra pausa
